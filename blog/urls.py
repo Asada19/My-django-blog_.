b0db3bd2f510c853c494a163fbefc73010024cb7
Asada18 <Livencor@gmail.com>
2021-01-08 00:01:36 +0600

    add comment

--- blog/urls.py
+++ blog/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogging/', include('main.urls'))
+    path('blogging/', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
