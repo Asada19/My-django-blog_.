55e857f3e9aa7256fbe0e57fd84ef6a368751e38
Asada18 <Livencor@gmail.com>
2021-01-08 13:46:07 +0600

    new version2 added some fiches

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from .models import *
+
 
 def index(request):
     return render(request, 'index.html')
 
 
 def category_detail(request, slug):
-
-    return render(request, 'category-detail.html')
+    category = Category.objects.get(slug=slug)
+    return render(request, 'category-detail.html', {'category': category})
