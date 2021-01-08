f5421c58baeda1e3676af0fa62b1bc08970366db
Asada18 <Livencor@gmail.com>
2021-01-08 09:58:36 +0600

    new version

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ImageInlineAdmin(admin.TabularInline):
+    model = Image
+    fields = ('image', )
+    max_num = 5
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [ImageInlineAdmin, ]
+
+
+admin.site.register(Category)
+
